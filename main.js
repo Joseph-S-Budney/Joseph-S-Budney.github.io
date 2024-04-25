b3a949179e15e16051bc19b3ab58d88b7726e68f
Budney <Jo.S.Budney@baypath.net>
2024-04-25 08:22:55 -0400

    now using sveltekit

--- main.js
+++
@@ -1,5 +0,0 @@
-const circle = document.getElementById('circle');
-document.addEventListener('mousemove', function(e){
-    circle.style.left = e.clientX-(circle.offsetWidth/2) + 'px';
-    circle.style.top = e.clientY-(circle.offsetHeight/2) + 'px';
-});
