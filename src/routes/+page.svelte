073487fcc77af7ce8a8f8c0b8363f40b11ce503d
Budney <Jo.S.Budney@baypath.net>
2024-05-22 09:05:14 -0400

    centered, text next to typewriter, yellow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
-    import Typewriter from 'svelte-typewriter'
+	import Typewriter from 'svelte-typewriter';
 </script>
-<span class="mb-4 text-center leading-none md:text-5xl lg:text-6xl dark:text-white"><h1>Joseph Budney</h1></span>
-<div class="text-4xl text-center dark:text-white">
-    <Typewriter mode=loop --cursor-color=white interval=100 keepCursorOnFinish=true>
-        <h2>front-end</h2>
-        <h2>back-end</h2>
-        <h2>freelance</h2>
-        <h2>fun</h2>
-    </Typewriter>
+
+<div id="home-hero">
+	<span class="mb-4 text-center leading-none dark:text-white md:text-5xl lg:text-6xl"
+		><h1>Joseph Budney</h1></span
+	>
+	<div class="text-center leading-loose flex text-4xl dark:text-white">
+        <p class="">I do... &nbsp;</p>
+        <div class="text-yellow-600">
+            <Typewriter mode="loop" --cursor-color="white">
+                <h2> front-end</h2>
+                <h2> back-end</h2>
+                <h2> freelance</h2>
+                <h2> fun</h2>
+            </Typewriter>
+        </div>
+	</div>
 </div>
+
+<style>
+	#home-hero {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
