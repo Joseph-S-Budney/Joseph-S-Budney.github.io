39e31e00761a69039eebcf6d800efd41b30152c9
Budney <Jo.S.Budney@baypath.net>
2024-05-24 09:16:36 -0400

    added layout and elements for about me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import Typewriter from 'svelte-typewriter';
     import { Button } from '$lib/components/ui/button'
+    import * as Avatar from "$lib/components/ui/avatar";
+    import * as Card from "$lib/components/ui/card";
+    import * as Carousel from "$lib/components/ui/carousel/index.js";
     
     function handleAnchorClick (event: { preventDefault: () => void; currentTarget: any; }) {
         event.preventDefault();
@@ -19,7 +22,7 @@
 <ul class="flex">
     <li class="flex-1 mr-2">
         <a class="text-center block border border-border rounded py-2 px-4 bg-primary hover:bg-ring text-primary-foreground" 
-        href="#home-hero" on:click={handleAnchorClick}>Active Item</a>
+        href="#home-hero" on:click={handleAnchorClick}>Home</a>
     </li>
     <li class="flex-1 mr-2">
         <a class="text-center block border border-border rounded bg-secondary text-secondary-foreground hover:bg-muted-foreground py-2 px-4" 
@@ -27,7 +30,7 @@
     </li>
     <li class="text-center flex-1">
         <a class="text-center block border border-border rounded bg-secondary text-secondary-foreground hover:bg-muted-foreground py-2 px-4" 
-        href="#" on:click={handleAnchorClick}>Disabled Item</a>
+        href="#" on:click={handleAnchorClick}>Contact</a>
     </li>
 </ul>
 </nav>
@@ -42,22 +45,108 @@
             <p class="">I do... &nbsp;</p>
             <div class="text-primary">
                 <Typewriter mode="loop" --cursor-color="white">
-                    <h2> front-end</h2>
-                    <h2> back-end</h2>
-                    <h2> freelance</h2>
-                    <h2> fun</h2>
+                    <h2>front-end</h2>
+                    <h2>back-end</h2>
+                    <h2>freelance</h2>
+                    <h2>it all... except design</h2>
                 </Typewriter>
             </div>
         </div>
     </div>
-    <Button href="#about" on:click={handleAnchorClick} class="left-1/2 -translate-x-1/2 bottom-10 absolute">About Me</Button>
+    <Button href="#about" variant="ghost" on:click={handleAnchorClick} class="left-1/2 -translate-x-1/2 bottom-10 absolute">About Me</Button>
 </div>
-<!-- svelte-ignore a11y-missing-content -->
-<a id="about"></a>
-<h2 class="text-4xl">About Me</h2>
-<p class="w-4 mx-auto text-center">
 
-</p>
+<h2 id="about" class="text-4xl">About Me</h2>
+<div class="container grid grid-rows-3 grid-cols-3 gap-4">
+    <div class="col">
+        <Avatar.Root>
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+        </Avatar.Root>
+    </div>
+    <div>
+        <Card.Root>
+            <Card.Header>
+                <Card.Title>About Me</Card.Title>
+                <Card.Description>Card Description</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>Card Content</p>
+            </Card.Content>
+            <Card.Footer>
+                <p>Card Footer</p>
+            </Card.Footer>
+        </Card.Root>
+    </div>
+    <div class="row-span-2">
+        <Card.Root class="h-full">
+            <Card.Header>
+                <Card.Title>Skills</Card.Title>
+                <Card.Description>Card Description</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>Card Content</p>
+            </Card.Content>
+            <Card.Footer>
+                <p>Card Footer</p>
+            </Card.Footer>
+        </Card.Root>
+    </div>
+    <div class="col-span-2">
+        <Card.Root>
+            <Card.Header>
+                <Card.Title>Hobbies</Card.Title>
+                <Card.Description>Card Description</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>Card Content</p>
+            </Card.Content>
+            <Card.Footer>
+                <p>Card Footer</p>
+            </Card.Footer>
+        </Card.Root>
+    </div>
+    <div></div>
+    <div>
+        <Carousel.Root class="h-full">
+            <Carousel.Content>
+                <Carousel.Item>
+                    <Card.Root>
+                        <Card.Content>1</Card.Content>
+                    </Card.Root>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Card.Root>
+                        <Card.Content>2</Card.Content>
+                    </Card.Root>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Card.Root>
+                        <Card.Content>3</Card.Content>
+                    </Card.Root>
+                </Carousel.Item>
+            </Carousel.Content>
+            <Carousel.Previous />
+            <Carousel.Next />
+        </Carousel.Root>
+    </div>
+    <div></div>
+    <div class="col-span-3">
+        <Card.Root>
+            <Card.Header>
+                <Card.Title>My Story</Card.Title>
+                <Card.Description>Card Description</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>Card Content</p>
+            </Card.Content>
+            <Card.Footer>
+                <p>Card Footer</p>
+            </Card.Footer>
+        </Card.Root>
+    </div>
+
+</div>
 <style>
     #home-hero{
         height: 100vh;
